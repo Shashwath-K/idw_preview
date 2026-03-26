b2802a316fee1390046b5c37c77f37cd39c05229
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-03-26 18:52:57 +0530

    Template migrations page-3

---
+++ backend/routers/instructor_detail.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Query
+from backend.services import instructor_detail_service
+
+router = APIRouter(prefix="/instructor-detail", tags=["instructor-detail"])
+
+@router.get("/filters")
+def get_filters():
+    return instructor_detail_service.get_instructor_detail_filters()
+
+@router.get("/data")
+def get_data(
+    instructor_name: str | None = Query(None),
+    year: str | None = Query(None),
+    month: int | None = Query(None)
+):
+    return {"table": instructor_detail_service.get_instructor_detail_data(instructor_name, year, month)}
