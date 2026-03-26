231ba9f6b914b0649f5b55f9df6bd00922563ffa
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-03-26 18:53:25 +0530

    Template migrations page-6

---
+++ backend/routers/programwise_report.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Query
+from backend.services import programwise_report_service
+
+router = APIRouter(prefix="/programwise-report", tags=["programwise-report"])
+
+@router.get("/filters")
+def get_filters():
+    return programwise_report_service.get_programwise_report_filters()
+
+@router.get("/data")
+def get_data(
+    category: str | None = Query(None),
+    year: str | None = Query(None),
+    month: int | None = Query(None)
+):
+    return {"table": programwise_report_service.get_programwise_report_data(category, year, month)}
