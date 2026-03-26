b15f9d51dd4b0201ace7df3e55e9ca0989298f58
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-03-26 18:53:53 +0530

    Template migrations page-9

---
+++ backend/routers/work_day.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Query
+from backend.services import work_day_service
+
+router = APIRouter(prefix="/work-day", tags=["work-day"])
+
+@router.get("/filters")
+def get_filters():
+    return work_day_service.get_work_day_filters()
+
+@router.get("/data")
+def get_data(
+    region: str | None = Query(None),
+    area: str | None = Query(None),
+    year: str | None = Query(None),
+    month: int | None = Query(None)
+):
+    return {"table": work_day_service.get_work_day_data(region, area, year, month)}
