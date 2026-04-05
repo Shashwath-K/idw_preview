1f78bf25efdf068f502c1ff31b20b4f3d0799193
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-04-05 12:12:29 +0530

    CI-05: Staging local db migrations to new dbs

--- backend/config.py
+++ backend/config.py
@@ -9,19 +9,37 @@ STATIC_DIR = FRONTEND_DIR / "static"
 SQL_DIR = BASE_DIR / "sql"
 
 
+''' # For Production
 DB_USER = os.getenv("PRAMANA_DB_USER", "pramana")
 DB_PASSWORD = os.getenv("PRAMANA_DB_PASSWORD", "QbqokyOFxJoI4I9n9Ti8")
 DB_HOST = os.getenv("PRAMANA_DB_HOST", "192.168.113.15")
+'''
+# For Local Instance 
+DB_USER = os.getenv("PRAMANA_DB_USER", "postgres")
+DB_PASSWORD = os.getenv("PRAMANA_DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("PRAMANA_DB_HOST", "localhost")
 DB_PORT = os.getenv("PRAMANA_DB_PORT", "5432")
-DB_SSL_MODE = os.getenv("PRAMANA_DB_SSL_MODE", "require")
+DB_SSL_MODE = os.getenv("PRAMANA_DB_PORT", "disable")
 
+
+''' # For Production 
 ADMIN_DB_NAME = os.getenv("PRAMANA_ADMIN_DB_NAME", "pramanadb")
 SOURCE_DB_NAME = os.getenv("PRAMANA_SOURCE_DB_NAME", "pramanadb")
 DATAMART_DB_NAME = os.getenv("PRAMANA_DATAMART_DB_NAME", "pramanadb")
+'''
+
+# For Local Instance
+
+ADMIN_DB_NAME = os.getenv("PRAMANA_ADMIN_DB_NAME", "pramana_v4_db")
+SOURCE_DB_NAME = os.getenv("PRAMANA_SOURCE_DB_NAME", "pramana_v4_db")
+DATAMART_DB_NAME = os.getenv("PRAMANA_DATAMART_DB_NAME", "pramana_v4_db")
+
+
 
 # Default schema names
-SOURCE_SCHEMA_NAME = "source_data_schema"
-DATAMART_SCHEMA_NAME = "dw_data_schema"
+SOURCE_SCHEMA_NAME = "source"
+DATAMART_SCHEMA_NAME = "dw"
+
 
 # If source and datamart are in the same DB, don't use FDW alias, use real schema name
 default_fdw_schema = SOURCE_SCHEMA_NAME if SOURCE_DB_NAME == DATAMART_DB_NAME else "source_fdw"
